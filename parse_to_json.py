a1e5f664a549eb9e28385db3184c0f4e1b735a95
Dante <d.niewenhuis@hotmail.com>
2017-10-19 14:08:04 +0200

    cleaned parse_to_json, still needs more comments

--- parse_to_json.py
+++ parse_to_json.py
@@ -10,71 +10,78 @@ def clean_text(s):
     s = re.sub("\W\W+", " ", s) # remove multiple spaces
     return s
 
-def parse_posts(all_rows, categorie, result, parent_dict):
+def parse_posts(all_rows, categorie, result_dict, parent_dict):
     accepted_answer_doc = {}
 
     for row in all_rows:
 
         if row.get('posttypeid') == '1': # check if row is a post
-            row_id = row.get('id')
-            doc_id = categorie + row_id # create a doc_id
-            try:
-                result_dict[doc_id]['answers']
-            except:
-                result[doc_id] = {"answers":"", "comments":"", "answer_score":0}
-
-            try: 
-                accepted_answer_doc[categorie + row.get("acceptedanswerid")] = doc_id
-            except: pass
-
-            # get all information of the post
-            result[doc_id]["categorie"] = categorie
-            result[doc_id]["title"] = row.get('title')
-            result[doc_id]["tags"] = row.get('tag')
-            result[doc_id]["body"] = clean_text(row.get('body'))
-            result[doc_id]["viewcount"] = row.get('viewcount')
-            result[doc_id]["score"] = row.get('score')
-            result[doc_id]["creation_date"] = row.get('creationdate')
-            result[doc_id]["link"] = categorie + ".stackexchange.com/questions/" + row_id + "/" \
-                                    + result[doc_id]["title"].replace(" ", "-")
+            parse_question(row, categorie, result_dict, parent_dict, accepted_answer_doc)
 
         elif row.get('posttypeid') == '2':
-            doc_id = categorie + row.get('parentid')
-            row_id = categorie + row.get('id')
-            parent_dict[row_id] = doc_id
-            
-            if row_id in accepted_answer_doc:
-                try:
-                    result[doc_id]["accepted_answer"] = clean_text(row.get('body'))
-                    result[doc_id]["accepted_answer_score"] = int(row.get('score'))
+            parse_answer(row, categorie, result_dict, parent_dict, accepted_answer_doc)            
 
-                except:
-                    result[doc_id] = {"answers":"", "comments":"", "answer_score":0}
-                    result[doc_id]["accepted_answer"] = clean_text(row.get('body'))
-                    result[doc_id]["accepted_answer_score"] = int(row.get('score'))
+def parse_question(row, categorie, result_dict, parent_dict, accepted_answer_doc):
+    row_id = row.get('id')
+    doc_id = categorie + row_id # create a doc_id
+    try:
+        result_dict[doc_id]['answers']
+    except:
+        result_dict[doc_id] = {"answers":"", "comments":"", "answer_score":0}
 
-            else:
-                try:
-                    result[doc_id]["answers"] += clean_text(row.get('body'))  
-                    result[doc_id]["answer_score"] += int(row.get('score'))
+    try: 
+        accepted_answer_doc[categorie + row.get("acceptedanswerid")] = doc_id
+    except: pass
 
-                except:
-                    result[doc_id] = {"answers":"", "comments":"", "answer_score":0}
-                    result[doc_id]["answers"] += clean_text(row.get('body'))  
-                    result[doc_id]["answer_score"] += int(row.get('score'))
+    # get all information of the post
+    result_dict[doc_id]["categorie"] = categorie
+    result_dict[doc_id]["title"] = row.get('title')
+    result_dict[doc_id]["tags"] = row.get('tag')
+    result_dict[doc_id]["body"] = clean_text(row.get('body'))
+    result_dict[doc_id]["viewcount"] = row.get('viewcount')
+    result_dict[doc_id]["score"] = int(row.get('score'))
+    result_dict[doc_id]["creation_date"] = row.get('creationdate')
+    result_dict[doc_id]["link"] = categorie + ".stackexchange.com/questions/" + row_id + "/" \
+                                    + result_dict[doc_id]["title"].replace(" ", "-")
 
-def parse_comments(all_rows, categorie, result, parent_dict):
+
+def parse_answer(row, categorie, result_dict, parent_dict, accepted_answer_doc):
+    doc_id = categorie + row.get('parentid')
+    row_id = categorie + row.get('id')
+    parent_dict[row_id] = doc_id
+    
+    if row_id in accepted_answer_doc:
+        try:
+            result_dict[doc_id]["accepted_answer"] = clean_text(row.get('body'))
+            result_dict[doc_id]["accepted_answer_score"] = int(row.get('score'))
+
+        except:
+            result_dict[doc_id] = {"answers":"", "comments":"", "answer_score":0}
+            result_dict[doc_id]["accepted_answer"] = clean_text(row.get('body'))
+            result_dict[doc_id]["accepted_answer_score"] = int(row.get('score'))
+
+    else:
+        try:
+            result_dict[doc_id]["answers"] += clean_text(row.get('body'))  
+            result_dict[doc_id]["answer_score"] += int(row.get('score'))
+
+        except:
+            result_dict[doc_id] = {"answers":"", "comments":"", "answer_score":0}
+            result_dict[doc_id]["answers"] += clean_text(row.get('body'))  
+            result_dict[doc_id]["answer_score"] += int(row.get('score'))
+
+def parse_comments(all_rows, categorie, result_dict, parent_dict):
     for row in all_rows:
         doc_id = categorie + row.get('postid')
 
         try:
-            result[doc_id]["comments"] += clean_text(row.get('text'))
+            result_dict[doc_id]["comments"] += clean_text(row.get('text'))
 
         except:
             try:
                 doc_id = parent_dict[doc_id]
-                result[doc_id]["comments"] += clean_text(row.get('text'))
-                result[doc_id]["answer_score"] += int(row.get('score'))
+                result_dict[doc_id]["comments"] += clean_text(row.get('text'))
+                result_dict[doc_id]["answer_score"] += int(row.get('score'))
 
             except:
                 print(doc_id)
@@ -82,7 +89,7 @@ def parse_comments(all_rows, categorie, result, parent_dict):
             
 def dict_to_json(file , result_dict):
     with open(file, 'w') as outfile:  
-        json.dump(result_dict, outfile) # REMOVE INDENT LATER!!!!
+        json.dump(result_dict, outfile, indent=4) # REMOVE INDENT LATER!!!!
 
 def get_soup_from_xml(file):
     xml_file = open(file, 'r')
