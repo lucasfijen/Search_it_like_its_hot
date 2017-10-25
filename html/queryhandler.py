a81b8ea69c0565e3639724a22ef729d468f6bb29
Lucas <lucas.fijen@gmail.com>
2017-10-25 17:16:16 +0200

    Fixed a lot of html and query handling stuff

--- html/queryhandler.py
+++ html/queryhandler.py
@@ -1,27 +1,37 @@
-
+import testsearch
 
 
 # Convert input_text to prepare for request
 def query_handler(input_text):
-    testdiv = {'title': 'Hallo ik ben lucas wat wil je hebben?',
-               'link': 'http://nu.nl',
-               'content': input_text}
-    htmltext = create_div_from_dict(testdiv)
+    htmltext = ""
+    # Add here extra data that needs to be showed above
+
+    results = testsearch.search()
+
+    for article in results['hits']['hits']:
+        htmltext += create_div_from_dict(article)
     return htmltext
 
 
 # creates a single div for a articledict
-def create_div_from_dict(article_dict):
+def create_div_from_dict(article):
+    article_dict = article['_source']
     resultstring = "<div id='resultdiv'>"
     if 'title' in article_dict:
-        resultstring += '<h1>' + article_dict['title'] + '</h1>'
+        if 'link' in article_dict:
+            resultstring += '<a href="http://' + article_dict['link'] + \
+                            '" target="_blank">' + article_dict['title'] + '</a>'
+        else:
+            resultstring += '<h1>' + article_dict['title'] + '</h1>'
 
-    if 'link' in article_dict:
-        resultstring += '<p>' + article_dict['link'] + '</h1>'
-
-    if 'content' in article_dict:
+    if 'body' in article_dict:
         # Something to only take the first x words
-        resultstring += '<p>' + article_dict['content'] + '</p>'
+        splittext = article_dict['body'].split(' ')
+        text = " ".join(splittext[:100])
+        resultstring += '<p>' + text
+        if(len(splittext) > 100):
+            resultstring += '...'
+        resultstring += '</p>'
 
     resultstring += '</div>'
 
--- html/server.py
+++ html/server.py
@@ -18,11 +18,10 @@ def helloa(name):
 def handlerhandler():
     if request.method == 'POST':
         text = request.form.get('questionbar')
-        # return text
         result = handle.query_handler(text)
         return result
     else:
-        return 'ELSE'
+        return 'ERROR'
 
 
 @app.route('/')
