0eb043812d3ae0aebe03ce494ac6c3d5ea4039c8
Lucas <lucas.fijen@gmail.com>
2017-10-25 13:30:06 +0200

    Adited a lot of stylesheet for homepage

---
+++ html/queryhandler.py
@@ -0,0 +1,11 @@
+
+
+
+# Convert input_text to prepare for request
+def query_handler(input_text):
+    return htmltext
+
+
+# creates a single div for a articledict
+def create_div_from_dict(article_dict):
+    return single_div
---
+++ html/server.py
@@ -0,0 +1,33 @@
+# first to run in terminal: FLASK_APP=test.py flask run
+# Tutorial: http://flask.pocoo.org/docs/0.12/quickstart/#a-minimal-application
+# http://localhost:5000/hello/Lucas%20hahaha%20hahaha will work for this code
+#
+# https://codehandbook.org/python-flask-jquery-ajax-post/
+from flask import Flask, request, jsonify
+from flask import render_template
+import queryhandler as handle
+
+app = Flask(__name__)
+
+@app.route("/hello/<name>")
+def helloa(name):
+    return "GA WEG %s" % name
+
+
+@app.route('/tryout', methods=['POST'])
+def handlerhandler():
+    if request.method == 'POST':
+        text = request.form.get('questionbar')
+        return text
+        # result = handle.query_handler(text)
+        # return result
+    else:
+        return 'ELSE'
+
+
+@app.route('/')
+def testpage():
+    return render_template('test.html')
+
+if __name__ == "__main__":
+    app.run()
